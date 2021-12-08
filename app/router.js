26e37ae9ce3fb71b19776f0adc19aa39fb1bd22e
Etudiant O'clock <pnpvdc@gmail.com>
2021-12-08 13:36:16 +0100

    ETAPE 5

--- app/router.js
+++ app/router.js
@@ -6,25 +6,30 @@ const games = require('./data/games.json');
 
 //! tester si ça existe ou pas ? locals ? doc express 
 router.get('/', (request, response) => {
-    
+
     response.render('index', {
         isDiceRoller: false,
         games: games,
     });
-  });
+});
 
-router.get('/game/fourchette',(request, response) => {
-    response.render('fourchette', {
-        isDiceRoller: false,
-        games: games,
-    });
-  });
-
-router.get('/game/diceRoller',(request, response) => {
-    response.render('diceRoller', {
-        isDiceRoller: true,
-        games: games,
-    });
-  });
+router.get('/game/:nomDuJeu', (request, response) => {
+    if (request.params.nomDuJeu === 'diceRoller') {
+        response.render('diceRoller', {
+            isDiceRoller: true,
+            games: games,
+        });
+    } else if (request.params.nomDuJeu === 'fourchette') {
+            response.render('fourchette', {
+                isDiceRoller: false,
+                games: games,
+            });
+    } else {
+        response.render('notFound', {
+            isDiceRoller: false,
+            games: games,
+        })
+    }
+});
 
 module.exports = router;
