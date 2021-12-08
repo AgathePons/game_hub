543f94ee8d11b2549ddb225e7d0c714e32c1328e
Etudiant O'clock <pnpvdc@gmail.com>
2021-12-08 10:59:49 +0100

    ETAPE 3

--- app/router.js
+++ app/router.js
@@ -2,7 +2,22 @@ const express = require("express");
 const router = express.Router();
 
 router.get('/', (request, response) => {
-    response.render('index');
+    response.render('index', {
+        isDiceRoller: false
+    });
   });
+
+router.get('/game/fourchette',(request, response) => {
+    response.render('fourchette', {
+        isDiceRoller: false
+    });
+  });
+
+router.get('/game/diceRoller',(request, response) => {
+    response.render('diceRoller', {
+        isDiceRoller: true
+    });
+  });
+
 
 module.exports = router;
--- index.js
+++ index.js
@@ -5,11 +5,12 @@ const app = express();
 const port = 3000;
 
 // Lier ejs et lui indiquer où chercher les views
-app.set('views', './views');
+app.set('views', './app/views');
 app.set('view engine', 'ejs'); 
 
 // lui donner l'endroit des fichiers statiques
 app.use(express.static('public'));
+app.use(router);
 
 // l'écoute du serveur
 app.listen(port, () => {
