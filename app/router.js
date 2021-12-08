58fd28730b41114da630d00fcdd1ec47454f0183
Etudiant O'clock <pnpvdc@gmail.com>
2021-12-08 12:05:15 +0100

    ETAPE 4

--- app/router.js
+++ app/router.js
@@ -1,23 +1,30 @@
 const express = require("express");
 const router = express.Router();
 
+//* Récupérer le fichier games.json
+const games = require('./data/games.json');
+
+//! tester si ça existe ou pas ? locals ? doc express 
 router.get('/', (request, response) => {
+    
     response.render('index', {
-        isDiceRoller: false
+        isDiceRoller: false,
+        games: games,
     });
   });
 
 router.get('/game/fourchette',(request, response) => {
     response.render('fourchette', {
-        isDiceRoller: false
+        isDiceRoller: false,
+        games: games,
     });
   });
 
 router.get('/game/diceRoller',(request, response) => {
     response.render('diceRoller', {
-        isDiceRoller: true
+        isDiceRoller: true,
+        games: games,
     });
   });
-
 
 module.exports = router;
--- index.js
+++ index.js
@@ -17,4 +17,3 @@ app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
   });
 
-//TODO 
